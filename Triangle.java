1913d04fd92555104b72f1b467104e8a285c9c0b
Aditya746 <49762864+Aditya746@users.noreply.github.com>
2020-08-15 22:06:46 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,36 @@
+//Write a program, creating a Triangle class which contains a method that takes three parameters of the three sides of the triangle, ‘a’, ‘b’, ‘c’, 
+//and returns you with the area of the triangle and print it. 
+
+
+import java.util.*;
+
+public class Triangle{
+
+    public static double TriangleArea(double a, double b, double c)
+    {
+        double area;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter side a: ");
+        a=sc.nextDouble();
+        System.out.print("Enter side b: ");
+        b=sc.nextDouble();
+        System.out.print("Enter side c: ");
+        c=sc.nextDouble();
+        area= (b*c)/2;
+        return area;
+    }
+
+    public static void main(String []args){
+
+    double a=0;
+    double b=0;
+    double c= 0;
+    double area;
+
+        area= TriangleArea(a,b,c);
+        System.out.println("Area of Triangle is :  " + area);
+
+    }
+}
